ad9e8a95a46a90dbe674f284c7f07e04ebc357ad
Jason Rivera <jp_rivera_@hotmail.com>
2021-12-22 18:16:33 -0800

    fixed the height of FlatList using flex

--- App.js
+++ App.js
@@ -68,8 +68,17 @@ const App = () => {
 };
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff'
+  },
   content: {
-    margin: 40
+    padding: 40,
+    flex: 1
+  },
+  list: {
+    marginTop: 20,
+    flex: 1
   }
 });
 
